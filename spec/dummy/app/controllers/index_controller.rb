70ed1dd3ccb42f88294def3e5f1c83307b96c8da
jafrog <howeveririna@gmail.com>
2013-01-03 00:21:08 +0400

    Interface for FX rates

--- spec/dummy/app/controllers/index_controller.rb
+++ spec/dummy/app/controllers/index_controller.rb
@@ -1,5 +1,14 @@
+require 'FXrate'
+require 'active_record_storage'
+
 class IndexController < ApplicationController
   def show
-    @currencies = [Struct.new(:id, :name).new(1, "USD")]
+    FXrate.setup :storage => ActiveRecordStorage.new
+    @currencies = ForeignExchangeRate.all.map(&:currency)
+
+    if params[:date]
+      @result = FXrate.at(Date.parse(params[:date]), params[:from], params[:to])
+      @result = @result * params[:amount].to_f if @result
+    end
   end
 end
